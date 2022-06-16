e5f63bfa7bf2614704ecc01d06ec3e025d76d5b4
usuda <you@example.com>
2022-06-17 01:06:36 +0900

    seeder追加

--- database/seeders/UserDataSeeder.php
+++ database/seeders/UserDataSeeder.php
@@ -15,7 +15,9 @@ class UserDataSeeder extends Seeder
     public function run()
     {
         DB::table('user_datas')->insert([
-            ['id' => 1, 'user_id' => 1, 'stripe_id' => '', 'point' => 0],
+            ['id' => 1, 'user_id' => 1, 'stripe_id' => '', 'point' => 0, 'rank' => 1],
+            ['id' => 2, 'user_id' => 2, 'stripe_id' => '', 'point' => 5000, 'rank' => 5],
+            ['id' => 3, 'user_id' => 3, 'stripe_id' => 'cus_HUZrWhauomejio', 'point' => 100000, 'rank' => 1],
         ]);
     }
 }
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -15,7 +15,9 @@ class UserSeeder extends Seeder
     public function run()
     {
         DB::table('users')->insert([
-            ['id' => 1, 'name' => 'ゲスト', 'email' => 'gtSYULvc1FIXMO4@pseudo.twitter.com', 'password' => '1', 'status' => 2, 'image' => 'mdhYzvjTCyq6qPgzRdcgXxMwMmcN37uaQZi95TSh.png'],
+            ['id' => 1, 'name' => 'ゲスト', 'email' => 'gtSYULvc1FIXMO4@pseudo.twitter.com', 'password' => '1', 'profile' => null, 'twitter_url' => 'https://twitter.com/BLIVE77191685', 'status' => 2, 'image' => 'mdhYzvjTCyq6qPgzRdcgXxMwMmcN37uaQZi95TSh.png'],
+            ['id' => 2, 'name' => '【公式】B-LIVE', 'email' => 'BLIVE77191685@pseudo.twitter.com', 'password' => '1', 'profile' => 'とにかく赤字にならなければいい。', 'twitter_url' => null, 'status' => 2, 'image' => 'iq17OfCyvQAjuINKgnwiPNUAG0Eyjq5pYN4Hyps9.jpeg'],
+            ['id' => 3, 'name' => 'うっすー', 'email' => 'sumaburaness12@gmail.com', 'password' => '1', 'profile' => "※このサイトの管理人です。\nスマブラ配信してますー", 'twitter_url' => 'https://twitter.com/sumaburaness12', 'status' => 2, 'image' => 'IVPSIwr5K27CLFHlNhqs0dnALeVQFMXsMcdJvspX.png'],
         ]);
     }
 }
