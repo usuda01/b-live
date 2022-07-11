67785a90873fa4690cae37de53e54b48138c39e2
usuda <hiroshi0104@gmail.com>
2022-07-11 17:03:41 +0900

    サイトマップ調整

--- resources/views/sitemap/index.blade.php
+++ resources/views/sitemap/index.blade.php
@@ -3,6 +3,8 @@
 <sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     <sitemap>
         <loc>{{ config('app.url') }}/sitemap/movie.xml</loc>
+    </sitemap>
+    <sitemap>
         <loc>{{ config('app.url') }}/sitemap/page.xml</loc>
     </sitemap>
 </sitemapindex>
