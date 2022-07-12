5cc8355ee0494aa2114cc8db8eb3377280fd39b0
usuda <hiroshi0104@gmail.com>
2022-07-12 15:40:17 +0900

    イベント非表示

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,7 +9,9 @@
             </div>
 -->
 
+<!--
             <carousel-component></carousel-component>
+-->
 
             <div class="category-content">
                 <h2 class="main-title">人気のタイトル</h2>
