50a9e9fad775e5f5597c730234fd38b6d57f3ffa
usuda <hiroshi0104@gmail.com>
2023-04-18 23:54:50 +0900

    スマホでコメントする際に画面が見えなくなる問題の解消

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -32,8 +32,8 @@
     @endif
     <link rel="apple-touch-icon" href="/images/apple-touch-icon.png" />
     <link href="{{ mix('css/fonts.css') }}" rel="stylesheet">
-    <link href="{{ mix('css/app.css') }}?param=63" rel="stylesheet">
-    <link href="{{ mix('css/all.css') }}?param=63" rel="stylesheet">
+    <link href="{{ mix('css/app.css') }}?param=64" rel="stylesheet">
+    <link href="{{ mix('css/all.css') }}?param=64" rel="stylesheet">
     @if (Request::is('/'))
         <title>B-LIVE｜ゲームのライブ配信、ショート動画サイト</title>
     @else
@@ -238,9 +238,9 @@
             'apiToken' => \Auth::user()->api_token ?? null
         ]) !!};
     </script>
-    <script src="{{ mix('js/app.js') }}?param=63"></script>
-    <script src="{{ mix('js/all.js') }}?param=63"></script>
-    <script src="{{ mix('js/common.js') }}?param=63"></script>
+    <script src="{{ mix('js/app.js') }}?param=64"></script>
+    <script src="{{ mix('js/all.js') }}?param=64"></script>
+    <script src="{{ mix('js/common.js') }}?param=64"></script>
     @stack('scripts')
 </body>
 </html>
