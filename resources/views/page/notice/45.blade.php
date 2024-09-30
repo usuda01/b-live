69dd9b2e0c99ed64216b2ffdaafed4587ade017c
usuda <hiroshi0104@gmail.com>
2024-09-30 23:55:47 +0900

    イベント14の準備

---
+++ resources/views/page/notice/45.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+@section('title', '第１４弾 アマゾンギフト券１万円獲得イベント開催！' . ' - ')
+@section('content')
+    <div class="page-content">
+        @include('parts.page_menu')
+        <div class="main-content">
+            <h2>お知らせ</h2>
+            <div class="box notice-detail">
+                <div class="section">
+                    <h3>第１４弾 アマゾンギフト券１万円獲得イベント開催！</h3>
+                    <div class="date">2023.09.08</div>
+                    <div>
+                        <div>アマゾンギフト券総額１万円獲得イベントを開催いたします。詳細は<a href="/event14">こちらから</a></div>
+                    </div>
+                </div>
+           </div>
+        </div>
+    </div>
+@endsection
