e73140af1491dc9de80671b1a59827df7134ebb4
usuda <hiroshi0104@gmail.com>
2024-11-14 17:21:41 +0900

    お知らせに「ほんのきもち。」を追加

---
+++ resources/views/page/notice/48.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.app')
+@section('title', '【新機能のお知らせ】「ほんのきもち。」コイン自動付与システムの導入' . ' - ')
+@section('content')
+    <div class="page-content">
+        @include('parts.page_menu')
+        <div class="main-content">
+            <h2>お知らせ</h2>
+            <div class="box notice-detail">
+                <div class="section">
+                    <h3>【新機能のお知らせ】「ほんのきもち。」コイン自動付与システムの導入</h3>
+                    <div class="date">2024.11.16</div>
+                    <div>
+                        <div>いつもご利用いただきありがとうございます。</div>
+                        <div>この度、配信者の皆様への日頃の感謝と応援を込めた新機能「ほんのきもち。」を実装いたしました！</div>
+                        <div>&nbsp;</div>
+                        <div>「ほんのきもち。」とは、30分ごとにボーナスコイン10コインが自動的に付与される特典です。</div>
+                        <div>配信を続けることで、配信者の皆様へ "感謝のきもち" が定期的に届く仕組みとなっております。</div>
+                        <div>これからも配信を楽しんでいただきながら、皆様のご活動を応援させていただければと思っております。</div>
+                        <div>&nbsp;</div>
+                        <div>詳細は <a href="/page/benefits">配信者特典</a> から</div>
+                    </div>
+                </div>
+           </div>
+        </div>
+    </div>
+@endsection
