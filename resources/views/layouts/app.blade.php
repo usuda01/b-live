cf954d1dec03eaf1cdef510a6fb4419990e0de46
usuda <hiroshi0104@gmail.com>
2024-11-15 16:30:18 +0900

    ?param=81 に更新

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -32,8 +32,8 @@
     @endif
     <link rel="apple-touch-icon" href="/images/apple-touch-icon.png" />
     <link href="{{ mix('css/fonts.css') }}" rel="stylesheet">
-    <link href="{{ mix('css/app.css') }}?param=80" rel="stylesheet">
-    <link href="{{ mix('css/all.css') }}?param=80" rel="stylesheet">
+    <link href="{{ mix('css/app.css') }}?param=81" rel="stylesheet">
+    <link href="{{ mix('css/all.css') }}?param=81" rel="stylesheet">
     @if (Request::is('/'))
         <title>B-LIVE｜ゲームのライブ配信、ショート動画サイト</title>
     @else
@@ -238,9 +238,9 @@
             'apiToken' => \Auth::user()->api_token ?? null
         ]) !!};
     </script>
-    <script src="{{ mix('js/app.js') }}?param=80"></script>
-    <script src="{{ mix('js/all.js') }}?param=80"></script>
-    <script src="{{ mix('js/common.js') }}?param=80"></script>
+    <script src="{{ mix('js/app.js') }}?param=81"></script>
+    <script src="{{ mix('js/all.js') }}?param=81"></script>
+    <script src="{{ mix('js/common.js') }}?param=81"></script>
     @stack('scripts')
 </body>
 </html>
