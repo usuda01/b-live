5159d55efe3673ca1111593b4a8f24df2cb8f33a
usuda <hiroshi0104@gmail.com>
2026-05-14 12:25:49 +0900

    配信中のタイトル/サムネを変更できるようにする - iOS向けAPI（POST /api/stream/update）を追加

--- routes/api.php
+++ routes/api.php
@@ -103,6 +103,7 @@ Route::group(['middleware' => ['api']], function () {
         // 配信設定
         Route::get('stream/config', [StreamController::class, 'config']);
         Route::post('stream/start', [StreamController::class, 'start']);
+        Route::post('stream/update', [StreamController::class, 'update']);
         Route::post('stream/end', [StreamController::class, 'end']);
 
         // 配信予定（リマインド）
